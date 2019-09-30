1115774bfecf5f1dbbba125b61793684508d4bc2
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2019-09-30 15:05:50 +0200

    Fixed error handling in employee controller

--- controllers/employee.js
+++ controllers/employee.js
@@ -1,10 +1,10 @@
 const Employee = require("../models/employee");
+const { ForbiddenError, BadRequestError, ResourceNotFoundError } = require("../lib/errors");
 
 const add = async (req, res) => {
   try {
     if (req.employee.role !== "admin") {
-      res.status(401);
-      throw new Error("Not authorization");
+      throw new ForbiddenError();
     }
     let newEmployee = new Employee({
       fullName: req.body.fullName,
@@ -16,41 +16,37 @@ const add = async (req, res) => {
     await newEmployee.save();
     res.status(201).send("Employee created...");
   } catch (error) {
-    res.send(error.message);
+    res.status(error.statusCode).send(error.message);
   }
 };
 
 const getAll = async (req, res) => {
   try {
     if (req.employee.role !== "admin") {
-      res.status(401);
-      throw new Error("No authorization");
+      throw new ForbiddenError();
     }
 
     const employees = await Employee.find();
     res.send(employees);
   } catch (error) {
-    res.send(error.message);
+    res.status(error.statusCode).send(error.message);
   }
 };
 
 const update = async (req, res) => {
   try {
     if (req.employee.role !== "admin") {
-      res.status(401);
-      throw new Error("No Authorization");
+      throw new ForbiddenError();
     }
 
     if (!req.body.id) {
-      res.status(400);
-      throw new Error("Missing employee's id");
+      throw new BadRequestError();
     }
 
     const employeeToUpdate = await Employee.findById(req.body.id);
 
     if (!employeeToUpdate) {
-      res.status(404);
-      throw new Error("Employee does not exist");
+      throw new ResourceNotFoundError();
     }
 
     if (req.body.password) {
@@ -72,28 +68,26 @@ const update = async (req, res) => {
 
     res.send("Employee updated...");
   } catch (error) {
-    res.send(error.message);
+    res.status(error.statusCode).send(error.message);
   }
 };
 
 const remove = async (req, res) => {
   try {
     if (req.employee.role !== "admin") {
-      res.status(401);
-      throw new Error("No authorization");
+      throw new ForbiddenError();
     }
 
     const employeeToDelete = await Employee.findById(req.params.id);
 
     if (!employeeToDelete) {
-      res.status(404);
-      throw new Error("Employee does not exist");
+      throw new ResourceNotFoundError();
     }
 
     await Employee.findByIdAndDelete(req.params.id);
     res.send("Employee deleted...");
   } catch (error) {
-    res.send(error.message);
+    res.status(error.statusCode).send(error.message);
   }
 };
 
